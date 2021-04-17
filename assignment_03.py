26a2ee91a5eb74a4ef7d5593b76cab90df70e0e0
Kathrin Dörfler <doerfler.kathrin@gmail.com>
2021-04-17 12:13:01 +0200

    Update assignment_03.py

--- assignment_03.py
+++ assignment_03.py
@@ -1,7 +1,9 @@
-""" 3. Write a code that generates a nested list with 3d point coordinates to generate a 2D XY point grid
+""" 
+3. Write a code that generates a nested list with 3d point coordinates to generate a 2D XY point grid
 Expected result: list_points = [[x1,y1,z], [x1,y2,z],...[xn,yn,z]
 """
 
 x_size = 10
 y_size = 6
+
 ### insert code here
